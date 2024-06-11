2182ad259883f6c15ce41a3f7e32123ff1371a5d
Shashank Mishra <shhhshank@gmail.com>
2024-06-11 23:16:48 +0530

    Completed almost

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Campaigns from "./scenes/campaigns";
+import CampaignCreate from "./scenes/campaign-create";
+import CommunicationLogs from "./scenes/communication-logs";
+import Customers from "./scenes/customers";
+import CustomerCreate from "./scenes/customer-create";
+import Orders from "./scenes/orders";
+import OrdersCreate from "./scenes/orders-create";
+import Visits from "./scenes/visits";
+import VisitsCreate from "./scenes/visits-create";
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+
+              <Route path="/campaigns" element={<Campaigns />} />
+              <Route path="/campaigns/create" element={<CampaignCreate />} />
+              <Route path="/campaigns/communication-logs" element={<CommunicationLogs />} />
+
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/customers/create" element={<CustomerCreate />} />
+
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/orders/create" element={<OrdersCreate />} />
+
+              <Route path="/visits" element={<Visits />} />
+              <Route path="/visits/create" element={<VisitsCreate />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,58 @@
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import GoogleIcon from '@mui/icons-material/Google';
+
+import useGoogleAuth from '../../hooks/useGoogleAuth'
+
+import { getAuth } from 'firebase/auth'
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  const { user, isLoading, signIn, signOut } = useGoogleAuth()
+
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* SEARCH BAR */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+
+
+
+        <IconButton onClick={user ? signOut : signIn}>
+          <Typography variant="h6" color={'#ffffff'}>
+            {user ? "Logout" : "SignIn with Google"}
+          </Typography>
+          <GoogleIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
